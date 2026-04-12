d12652ac786f61fcad787aea7aedb867eedf8487
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-04-12 23:32:06 +0300

    tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
     resolve: {
+        
         alias: {
             "@ubs-platform/neolit/core": path.resolve(__dirname, "src/package/core/index.ts"),
             "@ubs-platform/neolit/jsx-runtime": path.resolve(__dirname, "src/package/jsx/jsx-runtime.ts"),
